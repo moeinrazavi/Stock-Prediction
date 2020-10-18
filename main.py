d1f85269888a8b725997a793050c5e2b22403a0b
Moein Razavi <36338609+moeinrazavi@users.noreply.github.com>
2020-10-18 12:03:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from agent import Agent
+import numpy as np
+
+a = None
+for line in sys.stdin:
+    row = line.split(',')
+    row = np.array([float(x.strip()) for x in row])
+    if not a:
+        a = Agent(len(row))
+
+    res = a.step(row)
+    print(f"{res[0].name} {res[1]}")
